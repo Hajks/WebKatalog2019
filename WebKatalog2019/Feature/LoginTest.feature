76221b71ae97744ba35c4782ef1b9d8fca3fbdad
Piotr Tuński <kontakt@piotrtunski.pl>
2019-09-17 08:32:03 +0200

    Made scenario and class for login panel

---
+++ WebKatalog2019/Feature/LoginTest.feature
@@ -0,0 +1,30 @@
+﻿Feature: LoginTest
+
+Scenario Outline: Successful Login by User
+Given I Navigate to the Login page 'https://apbeta.webkatalog.pl/Account/Logowanie?ReturnUrl=/'
+When I Login with Username '<Login>' and Password '<Password>' on the Login Page
+Then the page should contain element like this 'Reklamacje'
+
+Examples: 
+| Login  | Password |
+| demo11 | demo11   |
+| demo12 | demo12   |
+| demo13 | demo13   |
+
+Scenario Outline: Login with not valid login and password
+Given I Navigate to the Login page 'https://apbeta.webkatalog.pl/Account/Logowanie?ReturnUrl=/'
+When I Login with Username '<Login>' and Password '<Password>' on the Login Page
+Then the page should contain element like this 'Język'
+
+Examples: 
+| Login         | Password         |
+|               |                  |
+| notvalidlogin |                  |
+|               | notvalidpassword |
+| demo12        |                  |
+|               | demo12           |
+| demo12        | notvalidpassword |
+| notvalidlogin | demo12           |
+| notvalidlogin | notvalidpassword |
+
+
---
+++ WebKatalog2019/Steps/LoginTestSteps.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.IO;
+using System.Reflection;
+using System.Threading;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using TechTalk.SpecFlow;
+
+namespace WebKatalog2019
+{
+    [Binding]
+    public class LoginTestSteps
+    {
+        private IWebDriver _driver;
+
+        [Given(@"I Navigate to the Login page '(.*)'")]
+        public void GivenINavigateToTheLoginPage(string LoginPageUrl)
+        {
+            _driver = new ChromeDriver(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location));
+            _driver.Manage().Window.Maximize();
+            _driver.Url = LoginPageUrl;
+        }
+        
+        [When(@"I Login with Username '(.*)' and Password '(.*)' on the Login Page")]
+        public void WhenILoginWithUsenameAndPasswordOnTheLoginPage(string Login, string Password)
+        {
+            IWebElement LoginTextBox = _driver.FindElement(By.XPath("//*[@id='LoginInput']"));
+            LoginTextBox.SendKeys(Login);
+            IWebElement PasswordTextBox = _driver.FindElement(By.XPath("//*[@id='HasloInput']"));
+            PasswordTextBox.SendKeys(Password);
+            _driver.FindElement(By.XPath("//*[@id='panelLogowaniaContainer']/div[2]/form/input[2]")).Click();
+        }
+
+        [Then(@"the page should contain element like this '(.*)'")]
+        public void ThenThePageShouldContainElementLikeThis(string TextToFind)
+        {
+            Assert.True(_driver.PageSource.Contains(TextToFind));
+        }
+
+        [AfterScenario]
+        public void CloseBrowser()
+        {
+            _driver.Dispose();
+        }
+    }
+}
